014ad58b6e8dd4ba4812c14f6ae4b54ed993091d
zggsong <zggsong@foxmail.com>
2025-12-10 10:10:40 +0800

    chore

--- src/STranslate/Views/Pages/AboutPage.xaml.cs
+++ src/STranslate/Views/Pages/AboutPage.xaml.cs
@@ -1,9 +1,4 @@
-using CommunityToolkit.Mvvm.DependencyInjection;
-using STranslate.Core;
-using STranslate.Plugin;
 using STranslate.ViewModels.Pages;
-using System.Diagnostics;
-using System.Windows;
 
 namespace STranslate.Views.Pages;
 
@@ -18,24 +13,4 @@ public partial class AboutPage
     }
 
     public AboutViewModel ViewModel { get; }
-
-    private void OnRepoCopy(object sender, RoutedEventArgs e)
-    {
-        Utilities.SetText(RepoTextBox.Text);
-
-        Ioc.Default.GetRequiredService<ISnackbar>()
-            .ShowSuccess(Ioc.Default.GetRequiredService<Internationalization>().GetTranslation("CopySuccess"));
-    }
-
-    private void OnWebsiteRequest(object sender, RoutedEventArgs e) =>
-        Process.Start(new ProcessStartInfo(Constant.Website) { UseShellExecute = true });
-
-    private void OnSponsorRequest(object sender, RoutedEventArgs e) =>
-        Process.Start(new ProcessStartInfo(Constant.Sponsor) { UseShellExecute = true });
-
-    private void OnJoinGroupRequest(object sender, RoutedEventArgs e) =>
-        Process.Start(new ProcessStartInfo(Constant.Group) { UseShellExecute = true });
-
-    private void OnReportRequest(object sender, RoutedEventArgs e) =>
-        Process.Start(new ProcessStartInfo(Constant.Report) { UseShellExecute = true });
 }
